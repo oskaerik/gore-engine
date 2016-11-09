1f349b835bac606cd651aae593991963540692e6
Oskar Eriksson <oseriks@kth.se>
2016-11-09 09:20:28 +0100

    Updated the Core file

--- src/Core.java
+++ src/Core.java
@@ -1,14 +1,18 @@
 import org.newdawn.slick.*;
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
 
+import java.awt.Font;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * A 2D game written in Java using Slick2D.
+ * A game written using Java and Slick2D.
  * @author Oskar Eriksson and Gustave Rousselet
  * @version 0.1
  */
 public class Core extends BasicGame {
+    TrueTypeFont font;
 
     public Core(String title) {
         super(title);
@@ -16,7 +20,7 @@ public class Core extends BasicGame {
 
     @Override
     public void init(GameContainer gameContainer) throws SlickException {
-
+        font = new TrueTypeFont(new Font("Arial", Font.BOLD, 24), true);
     }
 
     @Override
@@ -26,12 +30,12 @@ public class Core extends BasicGame {
 
     @Override
     public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
-
+        font.drawString(250, 100, "Welcome!", Color.white);
     }
 
     public static void main(String[] args) {
         try {
-            AppGameContainer appgc = new AppGameContainer(new Core("Simple Slick Game"));
+            AppGameContainer appgc = new AppGameContainer(new Core("slick-game"));
             appgc.setDisplayMode(640, 480, false);
             appgc.start();
         } catch (SlickException ex) {
