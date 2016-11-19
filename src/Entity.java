b573c306f3915c7d57fd3cba1ed44207e6c459c0
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-11-19 13:19:00 +0100

    JavaDoc and code cleanup for Entity class.

--- src/Entity.java
+++ src/Entity.java
@@ -7,24 +7,26 @@ import java.awt.*;
 import java.util.ArrayList;
 
 /**
- * Entity is the parent class of things in the world like items and objects
+ * Entity is the parent class of the physical things that populate the world such as Item, Character
+ * etc.
+ *
  * @author Oskar Eriksson and Gustave Rousselet
  * @version 0.1
  */
 public class Entity {
     private String name;
+    private String type;
     private Rectangle rectangle;
     private ArrayList<Animation> animationArray;
-    private String type;
     private boolean frozen;
     private TrueTypeFont font;
 
-    /** Constructor of the entity class
-     * @param rectangle The entity's rectangle
-     * @param name Name of the entity
-     * @param description Description of the entity
-     * @param type The type of the entity
-     * @throws SlickException Generic exception
+    /**
+     *
+     * @param rectangle The rectangle of the entity.
+     * @param name The name of the entity.
+     * @param type The type of the entity.
+     * @throws SlickException Generic exception.
      */
     public Entity(Rectangle rectangle, String name, String type)
             throws SlickException {
@@ -38,25 +40,46 @@ public class Entity {
     }
 
     /**
-     * @return The entity rectangle
+     * Method used to get the rectangle of the entity.
+     *
+     * @return Rectangle the rectangle of the entity.
      */
     public Rectangle getRect() {
         return rectangle;
     }
 
-
     /**
-     * @return Name of the entity
+     * Method used to get the name of the entity.
+     *
+     * @return String name of the entity.
      */
     protected String getName() { return name; }
 
     /**
-     * @return The font of the entity
+     * Method used to get the Font of the entity.
+     *
+     * @return Font of the entity.
      */
     protected TrueTypeFont getFont() { return font; }
 
+    /**
+     * Method used to get if the entity is frozen or not.
+     *
+     * @return Boolean according to the current frozen/not frozen state of entity.
+     */
     protected boolean isFrozen() { return frozen; }
+
+    /**
+     * Method used to set an entity into the frozen state.
+     *
+     * @param value Boolean true/false for fronzen/not frozen.
+     */
     protected void setFrozen(boolean value) { frozen = value; }
 
+    /**
+     * Method used to get the animation array of an entity.
+     *
+     * @return ArrayList of animations which the entity has. Usually up/down/left/right/default.
+     */
     protected ArrayList<Animation> getAnimationArray() { return animationArray; }
 }
