6d6db36e5e085bc131829f9b0a345da8351ca0df
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-11-19 15:01:24 +0100

    Finish JavaDoc for Item class.

--- src/Item.java
+++ src/Item.java
@@ -9,23 +9,26 @@ import java.awt.Font;
 /**
  * The Item class, an Item object is a object that can be added to and dropped from the inventory.
  * NPC characters could want an item, or an item could enable a specific player ability.
+ *
  * @author Oskar Eriksson and Gustave Rousselet
  * @version 0.1
  */
 public class Item extends Entity {
+
     /**
-     * Constructor for Item class
-     * @param rectangle The rectangle of the item
-     * @param name The name of the item
-     * @throws SlickException Generic exception
+     * Constructor for Item class.
+     *
+     * @param rectangle The rectangle of the item.
+     * @param name The name of the item.
+     * @throws SlickException Generic exception.
      */
     public Item (Rectangle rectangle, String name) throws SlickException {
         super(rectangle, name, "item");
     }
 
     /**
-     * Draws highlighting on items that intersects with player's range
-     * @param playerRange The player's range circle
+     * Draws highlighting on items that intersects with player's range.
+     * @param playerRange The player's range circle.
      */
     public void hightlight(Circle playerRange) {
         if (getRect().intersects(playerRange)) {
